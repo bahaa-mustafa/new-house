437bd26aa4f08579052e4bc4e5af4764875485be
bahaa-mustafa <bahaamustafabadr@gmail.com>
2025-09-05 10:12:06 +0300

    add navbar responsive

--- vite-project/src/Navbar/Navbar.jsx
+++ vite-project/src/Navbar/Navbar.jsx
@@ -1,6 +1,7 @@
 import "./Navbar.css"
 import { Search } from 'lucide-react';
 import { User } from 'lucide-react';
+import { Menu } from 'lucide-react';
 
 export default function Navbar(){
     return (
@@ -11,6 +12,7 @@ export default function Navbar(){
                         <div className="navbar_logo">
                             <p>new<span>house</span> </p>
                         </div>
+                        <input type="checkbox" name="menu" id="menu" hidden/>
                         <div className="navbar_links">
                             <a href="#home">Home</a>
                             <a href="#about">about</a>
@@ -22,6 +24,7 @@ export default function Navbar(){
                             <a href="#">login <User size={20}/></a>
                             <a href="#"><Search size={20}/></a>
                         </div>
+                        <label className="menu" htmlFor="menu"><Menu/></label>
                     </div>
                 </div>
             </nav>
